581f046a065c09e8cacf0fbed82b6230301bf1b2
Salih Durak <76474317+78e49c@users.noreply.github.com>
2024-05-03 20:17:42 +0300

    Update AppComponents.kt

--- app/src/main/java/com/example/gorgullebelle/components/AppComponents.kt
+++ app/src/main/java/com/example/gorgullebelle/components/AppComponents.kt
@@ -1,12 +1,25 @@
 package com.example.gorgullebelle.components
 
 
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.gestures.ModifierLocalScrollableContainerProvider.value
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
@@ -15,16 +28,23 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -74,14 +94,18 @@ fun MyTextField(labelValue: String, painterResource: Painter) {
     }
 
     OutlinedTextField(
-        modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(20.dp)),
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(8.dp))
+            .background(colorResource(id = R.color.background)),
         label = { Text(text = labelValue, modifier = Modifier.clip(RoundedCornerShape(20.dp))) },
         colors = TextFieldDefaults.outlinedTextFieldColors(
             focusedBorderColor = colorResource(id = R.color.primary),
             focusedLabelColor = colorResource(id = R.color.primary),
             cursorColor = colorResource(id = R.color.primary),
-            // backgroundColor = colorResource(id = R.color.background)
-        ), keyboardOptions = KeyboardOptions.Default, value = textValue.value, onValueChange = {
+        ), keyboardOptions = KeyboardOptions.Default,
+        value = textValue.value,
+        onValueChange = {
             textValue.value = it
         },
         leadingIcon = {
@@ -91,11 +115,113 @@ fun MyTextField(labelValue: String, painterResource: Painter) {
 
 }
 
-@Preview
+
+
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun preview() {
-    MyTextField(
-        labelValue = stringResource(id = R.string.first_name)
-        , painterResource(id = R.drawable.ic_launcher_background, )
+fun PasswordTextField(labelValue: String, painterResource: Painter) {
+
+    val password = remember {
+        mutableStateOf("")
+    }
+
+    val passwordVisible = remember {
+        mutableStateOf(false)
+    }
+
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(8.dp))
+            .background(colorResource(id = R.color.background)),
+        label = { Text(text = labelValue) },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = colorResource(id = R.color.primary),
+            focusedLabelColor = colorResource(id = R.color.primary),
+            cursorColor = colorResource(id = R.color.primary),
+            // backgroundColor = colorResource(id = R.color.background)
+        ),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password ),
+        value = password.value,
+        onValueChange = {
+            password.value = it
+        },
+        leadingIcon = {
+                Icon(painter = painterResource,contentDescription = "")
+        },
+
+        trailingIcon = {
+            val iconImage = if (passwordVisible.value) {
+                Icons.Filled.Favorite
+            } else {
+                Icons.Filled.Star
+            }
+            var description = if(passwordVisible.value){
+                stringResource(id = R.string.hide_password)
+            } else {
+                stringResource(id = R.string.show_password)
+            }
+
+            IconButton(onClick = {passwordVisible.value = !passwordVisible.value})
+            {
+                Icon(imageVector = iconImage,contentDescription = description)
+            }
+
+        },
+        visualTransformation = if(passwordVisible.value) VisualTransformation.None
+        else PasswordVisualTransformation()
     )
+
+}
+
+@Composable
+fun CheckboxCompoment(value: String){
+    Row (modifier = Modifier
+        .fillMaxWidth()
+        .heightIn(56.dp)
+        ,verticalAlignment = Alignment.CenterVertically,
+        ){
+        val checkedState = remember {
+            mutableStateOf(false)
+        }
+        Checkbox(checked = checkedState.value,
+            onCheckedChange = {checkedState.value = !checkedState.value
+            })
+
+        ClickableTextComponent(value = value)
+    }
+
+}
+
+@Composable
+fun ClickableTextComponent(value: String){
+    val text1 = "By continuing you agree to our "
+    val text2 = "Terms of Service "
+    val text3 = "and "
+    val text4 = "Privacy Policy"
+
+    val annotatedString = buildAnnotatedString {
+        append(text1)
+        withStyle(style = SpanStyle(color = colorResource(id = R.color.purple_700))){
+            pushStringAnnotation(tag = text2, annotation = text2)
+            append(text2)
+        }
+        append(text3)
+        withStyle(style = SpanStyle(color = colorResource(id = R.color.purple_700))){
+            pushStringAnnotation(tag = text4, annotation = text4)
+            append(text4)
+        }
+
+    }
+    ClickableText(text = annotatedString, onClick ={offset ->
+
+     annotatedString.getStringAnnotations(offset,offset)
+         .firstOrNull()?.also { span ->
+             Log.d("ss","{$span}")
+         }
+ }
+ )
 }
