cc406ef00bdd73ec12d341721a6b2c5cb9159ad4
Salih Durak <76474317+78e49c@users.noreply.github.com>
2024-05-13 23:36:50 +0300

    Create SignInScreen.kt

---
+++ app/src/main/java/com/example/gorgullebelle/app/SignInScreen.kt
@@ -0,0 +1,13 @@
+package com.example.gorgullebelle.app
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.example.gorgullebelle.app.navigation.Route
+
+@Composable
+fun SignInScreen(navigate: (String) -> Unit = {}) {
+
+    Button(onClick = { navigate(Route.SignUpScreen.route)}) {
+        Text(text = "kdskfskı")
+    }
+}
