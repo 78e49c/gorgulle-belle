6a022a627128c1741bae5a6d042911a32d595c2c
Salih Durak <76474317+78e49c@users.noreply.github.com>
2024-05-14 18:30:41 +0300

    Update SignUpScreen.kt

--- app/src/main/java/com/example/gorgullebelle/app/SignUpScreen.kt
+++ app/src/main/java/com/example/gorgullebelle/app/SignUpScreen.kt
@@ -1,20 +1,16 @@
 package com.example.gorgullebelle.app
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -23,6 +19,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.gorgullebelle.R
+import com.example.gorgullebelle.app.navigation.Route
 import com.example.gorgullebelle.components.ButtonComponent
 import com.example.gorgullebelle.components.CheckboxCompoment
 import com.example.gorgullebelle.components.ClickableLoginTextComponent
@@ -30,6 +27,7 @@ import com.example.gorgullebelle.components.HeadingTextComponent
 import com.example.gorgullebelle.components.MyTextField
 import com.example.gorgullebelle.components.NormalTextComponent
 import com.example.gorgullebelle.components.PasswordTextField
+import com.example.gorgullebelle.components.gradientBackground
 
 
 @Composable
@@ -44,19 +42,24 @@ fun SignUpScreen(navigate: (String) -> Unit = {}) {
         Surface(
             modifier = Modifier
                 .fillMaxSize()
-                .gradientBackground().alpha(0.1f)
+                .gradientBackground()
+                .alpha(0.1f)
         ) {}
 
-        Column(Modifier.fillMaxSize().padding(28.dp)
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(28.dp)
 
         ) {
 
 
-
             NormalTextComponent(value = stringResource(id = R.string.hello))
-            HeadingTextComponent(value = stringResource(id = R.string.create_account))
+            HeadingTextComponent(value = stringResource(id =  R.string.sign_up))
 
-            Spacer(modifier = Modifier.alpha(0.0f).height(20.dp))
+            Spacer(modifier = Modifier
+                .alpha(0.0f)
+                .height(20.dp))
 
             Column(Modifier.wrapContentSize()) {
                 MyTextField(
@@ -71,14 +74,18 @@ fun SignUpScreen(navigate: (String) -> Unit = {}) {
                     , painterResource(id = R.drawable.sharp_person_24)
 
                 )
-                Spacer(modifier = Modifier.height(10.dp).background(Color.Transparent))
+                Spacer(modifier = Modifier
+                    .height(10.dp)
+                    .background(Color.Transparent))
 
                 MyTextField(
                     labelValue = stringResource(id = R.string.email)
                     , painterResource(id = R.drawable.baseline_email_24)
                 )
 
-                Spacer(modifier = Modifier.height(10.dp).background(Color.Transparent))
+                Spacer(modifier = Modifier
+                    .height(10.dp)
+                    .background(Color.Transparent))
 
                 PasswordTextField(
                     labelValue = stringResource(id = R.string.password)
@@ -90,8 +97,8 @@ fun SignUpScreen(navigate: (String) -> Unit = {}) {
 
                 ButtonComponent(value = stringResource(id = R.string.register))
 
-                ClickableLoginTextComponent()
-
+                ClickableLoginTextComponent(navigate = navigate, route = Route.SignInScreen.route, "Giriş yap " ,"Hesabın varsa ")
+                ClickableLoginTextComponent(navigate = navigate, route = Route.DashboardScreen.route," Geri dön ","",)
             }
 
 
@@ -105,18 +112,6 @@ fun SignUpScreen(navigate: (String) -> Unit = {}) {
 
 
 
-@Composable
-fun Modifier.gradientBackground(): Modifier = this
-    .background(
-        brush = Brush.linearGradient(
-            colors =  listOf(
-                colorResource(id = R.color.grad_3),
-                colorResource(id = R.color.grad_2),
-                colorResource(id = R.color.grad_1),
-                colorResource(id = R.color.grad_0)
-            )
-        )
-    )
 
 
 @Preview
