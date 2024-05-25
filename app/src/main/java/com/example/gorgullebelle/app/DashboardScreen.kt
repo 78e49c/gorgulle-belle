61915924e66d217017c02849739b429b4b08b2e3
Salih Durak <76474317+78e49c@users.noreply.github.com>
2024-05-26 02:17:37 +0300

    Update DashboardScreen.kt

--- app/src/main/java/com/example/gorgullebelle/app/DashboardScreen.kt
+++ app/src/main/java/com/example/gorgullebelle/app/DashboardScreen.kt
@@ -1,14 +1,27 @@
 package com.example.gorgullebelle.app
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.gorgullebelle.app.navigation.Route
 
 @Composable
 fun DashboardScreen(navigate: (String) -> Unit = {}) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    )
+    {
+        Button(onClick = { navigate(Route.SignUpScreen.route) }) {
+            Text(text = "giriş vs")
+        }
 
-    Button(onClick = { navigate(Route.SignUpScreen.route)}) {
-        Text(text = "kdskfskı")
+        Button(onClick = { navigate(Route.ExperienceScreen.route) }) {
+            Text(text = "konuşma")
+        }
     }
 }
