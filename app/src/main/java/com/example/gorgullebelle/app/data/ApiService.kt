da3a3145c1bc925b35d2e79c77e37f31e8782eca
Salih Durak <76474317+78e49c@users.noreply.github.com>
2024-05-28 03:49:00 +0300

    Create ApiService.kt

---
+++ app/src/main/java/com/example/gorgullebelle/app/data/ApiService.kt
@@ -0,0 +1,9 @@
+package com.example.gorgullebelle.app.data
+
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface ApiService {
+    @POST("v1/chat/completions")
+    suspend fun sendChatRequest(@Body json: String): String
+}
