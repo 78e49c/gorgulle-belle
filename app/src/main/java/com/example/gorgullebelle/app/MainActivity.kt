ab078b20c6a318381bbb4da670eeb66fadef4e1b
Salih Durak <76474317+78e49c@users.noreply.github.com>
2024-06-19 05:09:51 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/gorgullebelle/app/MainActivity.kt
+++ app/src/main/java/com/example/gorgullebelle/app/MainActivity.kt
@@ -3,21 +3,21 @@ package com.example.gorgullebelle.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.gorgullebelle.app.presentation.navigation.NavGraph
+import com.example.gorgullebelle.app.presentation.viewmodel.ChatManagerViewModel
 import com.example.gorgullebelle.ui.theme.GBTheme
-
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GBTheme {
-                NavGraph()
+                val chatManagerViewModel: ChatManagerViewModel = viewModel()
+                NavGraph(chatManagerViewModel = chatManagerViewModel)
             }
         }
     }
 }
-
 
 
