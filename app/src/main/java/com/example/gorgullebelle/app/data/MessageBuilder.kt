768bcb1e86ecea679dd3c836e0dcf27a435c95dc
Salih Durak <76474317+78e49c@users.noreply.github.com>
2024-06-20 19:53:27 +0300

    Update MessageBuilder.kt

--- app/src/main/java/com/example/gorgullebelle/app/data/MessageBuilder.kt
+++ app/src/main/java/com/example/gorgullebelle/app/data/MessageBuilder.kt
@@ -1,22 +1,23 @@
 package com.example.gorgullebelle.app.data
+
 object MessageBuilder {
     // Mesaj geçmişine kullanıcı, sistem ve asistan mesajlarını ekler
-    fun buildMessageHistory(messageHistory: List<Message>, userMessage: String, systemMessage: String, assistantMessage: String): List<Message> {
+    fun buildMessageHistory(messageHistory: List<Message>, userMessage: String?, systemMessage: String?, assistantMessage: String?): List<Message> {
         val updatedMessages = messageHistory.toMutableList()
 
         // Sistem mesajı boş değilse, listeye ekle
-        if (systemMessage.isNotEmpty()) {
-            updatedMessages.add(Message("system", systemMessage))
+        systemMessage?.let {
+            updatedMessages.add(Message("system", it))
         }
 
         // Kullanıcı mesajı boş değilse, listeye ekle
-        if (userMessage.isNotEmpty()) {
-            updatedMessages.add(Message("user", userMessage))
+        userMessage?.let {
+            updatedMessages.add(Message("user", it))
         }
 
         // Asistan mesajı boş değilse, listeye ekle
-        if (assistantMessage.isNotEmpty()) {
-            updatedMessages.add(Message("assistant", assistantMessage))
+        assistantMessage?.let {
+            updatedMessages.add(Message("assistant", it))
         }
 
         return updatedMessages
