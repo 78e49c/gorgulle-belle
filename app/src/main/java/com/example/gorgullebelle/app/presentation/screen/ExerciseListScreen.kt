280c43ef6daf140f405127ac5a2788a7e83919b5
Salih Durak <76474317+78e49c@users.noreply.github.com>
2024-06-22 02:47:17 +0300

    Update ExerciseListScreen.kt

--- app/src/main/java/com/example/gorgullebelle/app/presentation/screen/ExerciseListScreen.kt
+++ app/src/main/java/com/example/gorgullebelle/app/presentation/screen/ExerciseListScreen.kt
@@ -1,9 +1,10 @@
 package com.example.gorgullebelle.app.presentation.screen
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -12,15 +13,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.gorgullebelle.R
 import com.example.gorgullebelle.app.presentation.components.CustomTopAppBar
-import com.example.gorgullebelle.app.presentation.components.ExerciseListItem
+import com.example.gorgullebelle.app.presentation.components.ExerciseCardItem
 import com.example.gorgullebelle.app.presentation.navigation.Route
+
+data class ExerciseItem(val title: String, val imageResId: Int, val body: String, val route: String)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ExerciseListScreen(
-    navigate: (String) -> Unit = {},
-) {
-
+fun ExerciseListScreen(navigate: (String) -> Unit = {}) {
+    val exerciseItems = listOf(
+        ExerciseItem("Konu tespiti", R.drawable.konu_tespiti, "0/20", Route.ExerciseScreen.route),
+        ExerciseItem("Gereklilik tespiti", R.drawable.dogru_atama, "3/74", Route.ExerciseScreen.route),
+        ExerciseItem("Uygun eylem", R.drawable.dogru_eylem, "25/35", Route.ExerciseScreen.route)
+    )
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -36,60 +41,25 @@ fun ExerciseListScreen(
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
-
-            Column(
-                modifier = Modifier.fillMaxSize()
+            LazyColumn(
+                modifier = Modifier
+                    //.fillMaxSize()
+                    .padding(8.dp)
             ) {
-
-
-
-                LazyColumn(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(8.dp)
-                ) {
-                    item {
-                        ExerciseListItem(
-                            title = "Konu tespiti",
-                            imageResId = R.drawable.konu_tespiti,
-
-                            onClick = {
-                                navigate(Route.ExerciseScreen.route)
-                            }
-                        )
-                    }
-
-                    item {
-                        ExerciseListItem(
-                            title = "Gereklilik tespiti",
-                            imageResId = R.drawable.dogru_atama,
-
-                            onClick = {
-
-
-                            }
-                        )
-                    }
-
-                    item {
-                        ExerciseListItem(
-                            title = "Uygun eylem",
-                            imageResId = R.drawable.dogru_eylem,
-                            onClick = {
-
-                            }
-                        )
+                items(exerciseItems.chunked(2)) { rowItems ->
+                    Row(modifier = Modifier.padding()) {
+                        for (item in rowItems) {
+                            ExerciseCardItem(
+                                imageRes = item.imageResId,
+                                title = item.title,
+                                body = item.body,
+                                onButtonClick = { navigate(item.route) },
+                                modifier = Modifier.weight(1f)
+                            )
+                        }
                     }
                 }
             }
-
-
-
-
-
-        }
         }
     }
-
-
-
+}
