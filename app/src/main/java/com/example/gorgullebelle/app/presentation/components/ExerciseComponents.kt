ef2b8f263bea750b1ecaaf6b8e7afce10c505f0e
Salih Durak <76474317+78e49c@users.noreply.github.com>
2024-06-22 02:47:12 +0300

    Update ExerciseComponents.kt

--- app/src/main/java/com/example/gorgullebelle/app/presentation/components/ExerciseComponents.kt
+++ app/src/main/java/com/example/gorgullebelle/app/presentation/components/ExerciseComponents.kt
@@ -1,25 +1,35 @@
 package com.example.gorgullebelle.app.presentation.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.gorgullebelle.R
 import com.example.gorgullebelle.app.domain.model.Question
 
 @Composable
@@ -98,4 +108,74 @@ fun QuestionComponent(
         }
 
     }
+}
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ExerciseCardItem(
+    imageRes: Int,
+    title: String,
+    body: String,
+    onButtonClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .clickable { onButtonClick() }
+            .width(166.dp)
+
+    ) {
+        Column(
+            modifier = Modifier
+                .background(Color.LightGray)
+                .padding(8.dp)
+                .width(166.dp)
+                //.height(420.dp)
+        ) {
+
+            Image(
+                painter = painterResource(id = imageRes),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(150.dp)
+                    .width(166.dp)
+                    .clip(RoundedCornerShape(4.dp))
+            )
+
+
+            Row (
+                modifier = Modifier
+                    .width(166.dp)
+
+            )
+            {
+                Text(
+                    text = title,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(vertical = 4.dp)
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                Text(
+                    text = body,
+                    fontSize = 14.sp,
+                    modifier = Modifier.padding(vertical = 4.dp)
+                )
+            }
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewExerciseCardItem() {
+    ExerciseCardItem(
+        imageRes = R.drawable.konu_tespiti,
+        title = "Exercise Title",
+        body = "2/41",
+        onButtonClick = { /* Do something */ }
+    )
 }
