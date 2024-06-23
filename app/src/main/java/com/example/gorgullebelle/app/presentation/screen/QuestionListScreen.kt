b681e6ac8c461ec2e8c4245e5750c265b6cc8e59
Salih Durak <76474317+78e49c@users.noreply.github.com>
2024-06-23 03:12:53 +0300

    Update QuestionListScreen.kt

--- app/src/main/java/com/example/gorgullebelle/app/presentation/screen/QuestionListScreen.kt
+++ app/src/main/java/com/example/gorgullebelle/app/presentation/screen/QuestionListScreen.kt
@@ -1,19 +1,19 @@
 package com.example.gorgullebelle.app.presentation.screen
 
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.example.gorgullebelle.R
 import com.example.gorgullebelle.app.presentation.components.CustomTopAppBar
-import com.example.gorgullebelle.app.presentation.components.ExerciseCardItem
+import com.example.gorgullebelle.app.presentation.components.ExerciseItemList
 import com.example.gorgullebelle.app.presentation.navigation.Route
 import com.example.gorgullebelle.app.presentation.viewmodel.QuestionViewModel
 
@@ -25,11 +25,13 @@ fun QuestionListScreen(
     navigate: (String) -> Unit = {},
     questionViewModel: QuestionViewModel
 ) {
-    val exerciseItems = listOf(
+    val initialExerciseItems = listOf(
         ExerciseItem("Konu tespiti", R.drawable.konu_tespiti, "0", "konuTespiti", Route.ExerciseScreen.route),
         ExerciseItem("Gereklilik tespiti", R.drawable.dogru_atama, "0", "gereklilikTespiti", Route.ExerciseScreen.route),
         ExerciseItem("Uygun eylem", R.drawable.dogru_eylem, "0", "uygunEylem", Route.ExerciseScreen.route)
     )
+
+    var exerciseItems by rememberSaveable { mutableStateOf(initialExerciseItems) }
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -45,27 +47,15 @@ fun QuestionListScreen(
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
-            LazyColumn(
-                modifier = Modifier
-                    .padding(8.dp)
-            ) {
-                items(exerciseItems.chunked(2)) { rowItems ->
-                    Row(modifier = Modifier.padding()) {
-                        for (item in rowItems) {
-                            ExerciseCardItem(
-                                imageRes = item.imageResId,
-                                title = item.title,
-                                body = item.body,
-                                onButtonClick = {
-                                    questionViewModel.setConcept(item.concept)
-                                    navigate(item.route)
-                                },
-                                modifier = Modifier.weight(1f)
-                            )
-                        }
-                    }
+            ExerciseItemList(
+                exerciseItems = exerciseItems,
+                onItemClick = { item ->
+                    questionViewModel.setConcept(item.concept)
+                    // Move the clicked item to the top of the list
+                    exerciseItems = listOf(item) + exerciseItems.filter { it != item }
+                    navigate(item.route)
                 }
-            }
+            )
         }
     }
 }
