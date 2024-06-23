f61fb88f9dccee958525d66edd3ac06d8559e36a
Salih Durak <76474317+78e49c@users.noreply.github.com>
2024-06-23 23:09:46 +0300

    Update Question.kt

--- app/src/main/java/com/example/gorgullebelle/app/data/Question.kt
+++ app/src/main/java/com/example/gorgullebelle/app/data/Question.kt
@@ -1,8 +1,6 @@
 package com.example.gorgullebelle.app.data
 
 data class Question(
-    val concept: String,
-    val topic: String = "General",
     val explanation: String,
     val questionText: String,
     val choices: List<Choice>
