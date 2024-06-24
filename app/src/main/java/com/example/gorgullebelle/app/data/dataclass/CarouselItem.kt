0a4f58133c52f7150d525770391d1acd2c3690eb
Salih Durak <76474317+78e49c@users.noreply.github.com>
2024-06-24 07:44:52 +0300

    Create CarouselItem.kt

---
+++ app/src/main/java/com/example/gorgullebelle/app/data/dataclass/CarouselItem.kt
@@ -0,0 +1,9 @@
+package com.example.gorgullebelle.app.data.dataclass
+
+data class CarouselItem(
+    val id: Int,
+    val imageRes: Int,
+    val title: String,
+    val body: String,
+    val buttonText: String
+)
