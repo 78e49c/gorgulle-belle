333edd06d17d0b22e0c6f956ec24e68bbe85f3e2
Salih Durak <76474317+78e49c@users.noreply.github.com>
2024-06-25 07:56:31 +0300

    Update ProfileViewModel.kt

--- app/src/main/java/com/example/gorgullebelle/app/presentation/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/example/gorgullebelle/app/presentation/viewmodel/ProfileViewModel.kt
@@ -7,13 +7,15 @@ import androidx.lifecycle.ViewModel
 class ProfileViewModel : ViewModel() {
     private val _score = MutableLiveData(0)
     private val _topics = MutableLiveData<List<String>>(emptyList())
+    private val _systemMessageVisible = MutableLiveData(false)
+
     val score: LiveData<Int> get() = _score
+    val topics: LiveData<List<String>> get() = _topics
+    val systemMessageVisible: LiveData<Boolean> get() = _systemMessageVisible
 
     fun updateScore(delta: Int) {
         _score.value = (_score.value ?: 0) + delta
     }
-
-    val topics: LiveData<List<String>> get() = _topics
 
     fun addTopic(topic: String) {
         _topics.value = _topics.value?.toMutableList()?.apply { add(topic) }
@@ -22,6 +24,8 @@ class ProfileViewModel : ViewModel() {
     fun removeTopic(topic: String) {
         _topics.value = _topics.value?.toMutableList()?.apply { remove(topic) }
     }
+
+    fun setSystemMessageVisible(visible: Boolean) {
+        _systemMessageVisible.value = visible
+    }
 }
-
-
