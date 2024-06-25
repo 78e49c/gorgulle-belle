ab7ee781252270d97ea2d91a5ae18703e9c03bf1
Salih Durak <76474317+78e49c@users.noreply.github.com>
2024-06-25 06:27:24 +0300

    Update ProfileViewModel.kt

--- app/src/main/java/com/example/gorgullebelle/app/presentation/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/example/gorgullebelle/app/presentation/viewmodel/ProfileViewModel.kt
@@ -6,9 +6,22 @@ import androidx.lifecycle.ViewModel
 
 class ProfileViewModel : ViewModel() {
     private val _score = MutableLiveData(0)
+    private val _topics = MutableLiveData<List<String>>(emptyList())
     val score: LiveData<Int> get() = _score
 
     fun updateScore(delta: Int) {
         _score.value = (_score.value ?: 0) + delta
     }
+
+    val topics: LiveData<List<String>> get() = _topics
+
+    fun addTopic(topic: String) {
+        _topics.value = _topics.value?.toMutableList()?.apply { add(topic) }
+    }
+
+    fun removeTopic(topic: String) {
+        _topics.value = _topics.value?.toMutableList()?.apply { remove(topic) }
+    }
 }
+
+
