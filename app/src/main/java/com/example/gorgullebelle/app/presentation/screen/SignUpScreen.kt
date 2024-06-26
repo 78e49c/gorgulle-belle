6cf3ccfb03c9ffd7b0e9e0b3643db39ba456ed49
Salih Durak <76474317+78e49c@users.noreply.github.com>
2024-06-26 18:14:24 +0300

    Update SignUpScreen.kt

--- app/src/main/java/com/example/gorgullebelle/app/presentation/screen/SignUpScreen.kt
+++ app/src/main/java/com/example/gorgullebelle/app/presentation/screen/SignUpScreen.kt
@@ -9,13 +9,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.gorgullebelle.R
 import com.example.gorgullebelle.app.presentation.components.ButtonComponent
@@ -26,27 +29,29 @@ import com.example.gorgullebelle.app.presentation.components.MyTextField
 import com.example.gorgullebelle.app.presentation.components.NormalTextComponent
 import com.example.gorgullebelle.app.presentation.components.PasswordTextField
 import com.example.gorgullebelle.app.presentation.navigation.Route
-
+import com.example.gorgullebelle.app.presentation.viewmodel.UserViewModel
 
 @Composable
-fun SignUpScreen(navigate: (String) -> Unit = {}) {
+fun SignUpScreen(
+    userViewModel: UserViewModel,
+    navigate: (String) -> Unit = {}
+) {
+    var name by remember { mutableStateOf("") }
+    var surname by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
 
     Surface(
         modifier = Modifier
             .fillMaxSize()
-    )
-    {
-
+    ) {
         Column(
             Modifier
                 .fillMaxSize()
                 .padding(28.dp)
-
         ) {
-
-
             NormalTextComponent(value = stringResource(id = R.string.hello))
-            HeadingTextComponent(value = stringResource(id =  R.string.sign_up))
+            HeadingTextComponent(value = stringResource(id = R.string.sign_up))
 
             Spacer(modifier = Modifier
                 .alpha(0.0f)
@@ -54,33 +59,33 @@ fun SignUpScreen(navigate: (String) -> Unit = {}) {
 
             Column(Modifier.wrapContentSize(), horizontalAlignment = Alignment.CenterHorizontally) {
                 MyTextField(
-                    labelValue = stringResource(id = R.string.first_name)
-                    , painterResource(id = R.drawable.sharp_person_24)
+                    labelValue = stringResource(id = R.string.first_name),
+                    painterResource(id = R.drawable.sharp_person_24),
+                    onValueChange = { name = it }
                 )
 
                 Spacer(modifier = Modifier.height(10.dp))
 
                 MyTextField(
-                    labelValue = stringResource(id = R.string.last_name)
-                    , painterResource(id = R.drawable.sharp_person_24)
-
+                    labelValue = stringResource(id = R.string.last_name),
+                    painterResource(id = R.drawable.sharp_person_24),
+                    onValueChange = { surname = it }
                 )
-                Spacer(modifier = Modifier
-                    .height(10.dp)
-                    .background(Color.Transparent))
+
+                Spacer(modifier = Modifier.height(10.dp).background(Color.Transparent))
 
                 MyTextField(
-                    labelValue = stringResource(id = R.string.email)
-                    , painterResource(id = R.drawable.baseline_email_24)
+                    labelValue = stringResource(id = R.string.email),
+                    painterResource(id = R.drawable.baseline_email_24),
+                    onValueChange = { email = it }
                 )
 
-                Spacer(modifier = Modifier
-                    .height(10.dp)
-                    .background(Color.Transparent))
+                Spacer(modifier = Modifier.height(10.dp).background(Color.Transparent))
 
                 PasswordTextField(
-                    labelValue = stringResource(id = R.string.password)
-                    , painterResource(id = R.drawable.baseline_key_24)
+                    labelValue = stringResource(id = R.string.password),
+                    painterResource(id = R.drawable.baseline_key_24),
+                    onValueChange = { password = it }
                 )
 
                 Spacer(modifier = Modifier.height(16.dp))
@@ -92,37 +97,21 @@ fun SignUpScreen(navigate: (String) -> Unit = {}) {
                 ButtonComponent(
                     value = stringResource(id = R.string.register),
                     onClick = {
+                        userViewModel.signUp(name, surname, email, password)
                         navigate.invoke(Route.SignInScreen.route)
                     }
                 )
 
                 Spacer(modifier = Modifier.height(16.dp))
 
-                ClickableLoginTextComponent(navigate = navigate, route = Route.SignInScreen.route, "Giriş yap " ,"Hesabın varsa ")
+                ClickableLoginTextComponent(navigate = navigate, route = Route.SignInScreen.route, "Giriş yap ", "Hesabın varsa ")
 
                 Spacer(modifier = Modifier.height(16.dp))
 
-                ClickableLoginTextComponent(navigate = navigate, route = Route.DashboardScreen.route," Geri dön ","",)
+                ClickableLoginTextComponent(navigate = navigate, route = Route.DashboardScreen.route, "Geri dön ", "",)
             }
-
-
-
         }
-
-
-
     }
 }
 
 
-
-
-
-
-
-
-@Preview
-@Composable
-fun DefaultPreviewOfSignUpScreen() {
-    SignUpScreen()
-}
