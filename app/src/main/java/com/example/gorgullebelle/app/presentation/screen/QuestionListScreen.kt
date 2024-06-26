3b24fed60ee33a5568b5bfcb5c1ad89e9da96784
Salih Durak <76474317+78e49c@users.noreply.github.com>
2024-06-26 12:04:12 +0300

    Update QuestionListScreen.kt

--- app/src/main/java/com/example/gorgullebelle/app/presentation/screen/QuestionListScreen.kt
+++ app/src/main/java/com/example/gorgullebelle/app/presentation/screen/QuestionListScreen.kt
@@ -2,12 +2,20 @@ package com.example.gorgullebelle.app.presentation.screen
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -15,6 +23,7 @@ import com.example.gorgullebelle.R
 import com.example.gorgullebelle.app.presentation.components.CustomTopAppBar
 import com.example.gorgullebelle.app.presentation.components.QuestionItemList
 import com.example.gorgullebelle.app.presentation.navigation.Route
+import com.example.gorgullebelle.app.presentation.viewmodel.ProfileViewModel
 import com.example.gorgullebelle.app.presentation.viewmodel.QuestionViewModel
 
 data class ExerciseItem(val title: String, val imageResId: Int, val concept: String, val route: String)
@@ -23,7 +32,8 @@ data class ExerciseItem(val title: String, val imageResId: Int, val concept: Str
 @Composable
 fun QuestionListScreen(
     navigate: (String) -> Unit = {},
-    questionViewModel: QuestionViewModel
+    questionViewModel: QuestionViewModel,
+    profileViewModel: ProfileViewModel
 ) {
     val initialExerciseItems = listOf(
         ExerciseItem("Konu tespiti", R.drawable.konu_tespiti,  "konuTespiti", Route.QuestionScreen.route),
@@ -33,15 +43,36 @@ fun QuestionListScreen(
 
     var exerciseItems by rememberSaveable { mutableStateOf(initialExerciseItems) }
 
+    val expanded = remember { mutableStateOf(false) }
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
             CustomTopAppBar(
                 conversationTitle = "Kısıtlı anlama",
-                onBackPressed = { navigate(Route.DashboardScreen.route) }
+                onBackPressed = { navigate(Route.DashboardScreen.route) },
+                menuContent = {
+                    IconButton(onClick = { expanded.value = !expanded.value }) {
+                        Icon(Icons.Default.MoreVert, contentDescription = "Menu")
+                    }
+                    DropdownMenu(
+                        expanded = expanded.value,
+                        onDismissRequest = { expanded.value = false }
+                    ) {
+                        DropdownMenuItem(
+                            text = { Text("Konular") },
+                            onClick = {
+                                navigate(Route.AddTopicScreen.route)
+                                expanded.value = false
+                            }
+                        )
+                    }
+                }
             )
         }
     ) { innerPadding ->
+
+
         Surface(
             modifier = Modifier
                 .fillMaxSize()
