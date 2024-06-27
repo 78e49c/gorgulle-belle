534daa56830a51bfea3c74ee9d777a3710c64ff3
Salih Durak <76474317+78e49c@users.noreply.github.com>
2024-06-27 04:11:24 +0300

    Update UserViewModel.kt

--- app/src/main/java/com/example/gorgullebelle/app/presentation/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/gorgullebelle/app/presentation/viewmodel/UserViewModel.kt
@@ -2,30 +2,53 @@ package com.example.gorgullebelle.app.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+
+data class User(
+    val name: String,
+    val surname: String,
+    val email: String,
+    val password: String,
+    var score: Int = 0
+)
 
 class UserViewModel : ViewModel() {
 
-    val names = mutableListOf<String>()
-    val surnames = mutableListOf<String>()
-    val emails = mutableListOf<String>()
-    val passwords = mutableListOf<String>()
+    private val users = mutableListOf(
+        User("Giriş", "Yap", "puanlarini@kaydetmek.icin", "11111111")
+    )
+
+    private val _onlineUser = MutableStateFlow(users[0])
+    val onlineUser: StateFlow<User> get() = _onlineUser
 
     fun signUp(name: String, surname: String, email: String, password: String) {
         viewModelScope.launch {
-            names.add(name)
-            surnames.add(surname)
-            emails.add(email)
-            passwords.add(password)
+            val user = User(name, surname, email, password)
+            users.add(user)
         }
     }
 
     fun signIn(email: String, password: String): Pair<String, String>? {
-        val index = emails.indexOf(email)
-        return if (index != -1 && passwords[index] == password) {
-            Pair(names[index], surnames[index])
+        val userIndex = users.indexOfFirst { it.email == email && it.password == password }
+        return if (userIndex != -1 && _onlineUser.value == users[0]) {
+            _onlineUser.value = users[userIndex]
+            Pair(users[userIndex].name, users[userIndex].surname)
         } else {
             null
         }
+    }
+
+    fun signOut() {
+        _onlineUser.value = users[0]
+    }
+
+    fun updateScore(scoreChange: Int) {
+        val currentOnlineUser = _onlineUser.value
+        //if (currentOnlineUser != users[0]) {
+            currentOnlineUser.score += scoreChange
+            _onlineUser.value = currentOnlineUser
+        //}
     }
 }
